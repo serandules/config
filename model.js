6c94cb08bbd37e2917af0561d508c5b457460c45
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-10-25 19:38:14 +0530

    updating

--- model.js
+++ model.js
@@ -2,6 +2,7 @@ var log = require('logger')('model-configs');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var mongutils = require('mongutils');
 var mongins = require('mongins');
 var types = require('validators').types;
 
@@ -9,6 +10,7 @@ var config = Schema({
     name: {
         type: String,
         required: true,
+        searchable: true,
         validator: types.name({
             length: 100
         })
@@ -21,9 +23,17 @@ var config = Schema({
     }
 }, {collection: 'configs'});
 
-config.plugin(mongins);
+config.plugin(mongins({
+  transform: function (o) {
+      o.value = JSON.parse(o.value);
+  }
+}));
 config.plugin(mongins.user);
 config.plugin(mongins.createdAt());
 config.plugin(mongins.updatedAt());
+
+mongutils.ensureIndexes(config, [
+  {name: 1, createdAt: 1, _id: 1}
+]);
 
 module.exports = mongoose.model('configs', config);
