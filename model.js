de57b494e8278ef843803336e4292187802887b4
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-12-08 02:31:25 +0530

    updating

--- model.js
+++ model.js
@@ -2,37 +2,39 @@ var log = require('logger')('model-configs');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var mongutils = require('mongutils');
 var mongins = require('mongins');
-var types = require('validators').types;
+var validators = require('validators');
+var model = require('model');
+
+var types = validators.types;
 
 var config = Schema({
-    name: {
-        type: String,
-        required: true,
-        searchable: true,
-        validator: types.name({
-            length: 100
-        })
-    },
-    value: {
-        type: String,
-        validator: types.string({
-            length: 1000
-        })
-    }
+  name: {
+    type: String,
+    required: true,
+    searchable: true,
+    validator: types.name({
+      length: 100
+    })
+  },
+  value: {
+    type: String,
+    validator: types.string({
+      length: 1000
+    })
+  }
 }, {collection: 'configs'});
 
 config.plugin(mongins({
   transform: function (o) {
-      o.value = JSON.parse(o.value);
+    o.value = JSON.parse(o.value);
   }
 }));
 config.plugin(mongins.user);
 config.plugin(mongins.createdAt());
 config.plugin(mongins.updatedAt());
 
-mongutils.ensureIndexes(config, [
+model.ensureIndexes(config, [
   {name: 1, createdAt: 1, _id: 1}
 ]);
 
